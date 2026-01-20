b1d75e5f090a377c5170685df9713eee837a4e03
Mario Rivera <marioakamomo@gmail.com>
2026-01-20 14:45:53 -0600

    Initial theme setup

---
+++ colors/marios.lua
@@ -0,0 +1,1 @@
+require("marios").load()
---
+++ lua/palette.lua
@@ -0,0 +1,13 @@
+return {
+	bg = "none",
+	fg = "#f2f2f2",
+	red = "#ff0080",
+	green = "#79ff0f",
+	blue = "#2d70ff",
+	yellow = "#ffee00",
+	magenta = "#b449be",
+	cyan = "#66ccff",
+	Lgray = "#bbbbbb",
+	Dgray = "#414141",
+	orange = "#ffa600",
+}
