02dd9dbbbb19175ce0cf92ea251dce696589249c
Mario Rivera <marioakamomo@gmail.com>
2026-01-20 16:41:41 -0600

    more finessing lol

--- lua/marios/colors.lua
+++ lua/marios/colors.lua
@@ -1,119 +1,13 @@
-local M = {}
-
-function M.setup()
-	vim.cmd("hi clear")
-	if vim.fn.exists("syntax_on") then
-		vim.cmd("syntax reset")
-	end
-	vim.o.termguicolors = true
---vim.g.colors_name = "marios_theme"
-
-	local c = require("marios.palette").setup()
-	local highlights = {
-		-- Base
-		Normal = { fg = c.fg, bg = c.bg },
-		NormalFloat = { fg = c.fg, bg = c.bg },
-		NormalNC = { fg = c.fg, bg = c.bg },
-		Comment = { fg = c.gray, italic = true },
-		Constant = { fg = c.yellow },
-		String = { fg = c.green },
-		Character = { fg = c.green },
-		Number = { fg = c.magenta },
-		Boolean = { fg = c.magenta },
-		Float = { fg = c.magenta },
-		Identifier = { fg = c.blue },
-		Function = { fg = c.cyan },
-		Statement = { fg = c.red },
-		Conditional = { fg = c.red },
-		Repeat = { fg = c.red },
-		Label = { fg = c.red },
-		Operator = { fg = c.fg },
-		Keyword = { fg = c.red },
-		Exception = { fg = c.red },
-		PreProc = { fg = c.magenta },
-		Include = { fg = c.magenta },
-		Define = { fg = c.magenta },
-		Macro = { fg = c.magenta },
-		PreCondit = { fg = c.magenta },
-		Type = { fg = c.yellow },
-		StorageClass = { fg = c.yellow },
-		Structure = { fg = c.yellow },
-		Typedef = { fg = c.yellow },
-		Special = { fg = c.cyan },
-		SpecialChar = { fg = c.cyan },
-		Tag = { fg = c.blue },
-		Delimiter = { fg = c.fg },
-		SpecialComment = { fg = c.gray },
-		Debug = { fg = c.red },
-		Underlined = { underline = true },
-		Ignore = { fg = c.gray },
-		Error = { fg = c.red, bold = true },
-		Todo = { fg = c.yellow, bold = true },
-		-- UI Elements
-		ColorColumn = { bg = c.gray },
-		Cursor = { fg = c.bg, bg = c.fg },
-		CursorLine = { bg = "none" },
-		CursorColumn = { bg = "none" },
-		CursorLineNr = { fg = c.yellow, bold = true },
-		LineNr = { fg = c.gray },
-		SignColumn = { bg = c.bg },
-		StatusLine = { fg = c.fg, bg = c.gray },
-		StatusLineNC = { fg = c.gray, bg = c.bg },
-		VertSplit = { fg = c.gray },
-		WinSeparator = { fg = c.gray },
-		Pmenu = { fg = c.fg, bg = c.gray },
-		PmenuSel = { fg = c.bg, bg = c.blue },
-		PmenuSbar = { bg = c.gray },
-		PmenuThumb = { bg = c.fg },
-		TabLine = { fg = c.gray, bg = c.bg },
-		TabLineFill = { bg = c.bg },
-		TabLineSel = { fg = c.fg, bg = c.blue },
-		Visual = { bg = c.gray },
-		Search = { fg = c.bg, bg = c.yellow },
-		IncSearch = { fg = c.bg, bg = c.cyan },
-		MatchParen = { fg = c.yellow, bold = true },
-		-- Diagnostics
-		DiagnosticError = { fg = c.red },
-		DiagnosticWarn = { fg = c.yellow },
-		DiagnosticInfo = { fg = c.cyan },
-		DiagnosticHint = { fg = c.gray },
-		DiagnosticUnderlineError = { undercurl = true, sp = c.red },
-		DiagnosticUnderlineWarn = { undercurl = true, sp = c.yellow },
-		DiagnosticUnderlineInfo = { undercurl = true, sp = c.cyan },
-		DiagnosticUnderlineHint = { undercurl = true, sp = c.gray },
-		-- TreeSitter
-		["@variable"] = { fg = c.fg },
-		["@variable.builtin"] = { fg = c.magenta },
-		["@variable.parameter"] = { fg = c.fg },
-		["@variable.member"] = { fg = c.blue },
-		["@constant"] = { fg = c.yellow },
-		["@constant.builtin"] = { fg = c.magenta },
-		["@module"] = { fg = c.blue },
-		["@string"] = { fg = c.green },
-		["@number"] = { fg = c.magenta },
-		["@boolean"] = { fg = c.magenta },
-		["@function"] = { fg = c.cyan },
-		["@function.builtin"] = { fg = c.cyan },
-		["@function.method"] = { fg = c.cyan },
-		["@constructor"] = { fg = c.yellow },
-		["@keyword"] = { fg = c.red },
-		["@keyword.function"] = { fg = c.red },
-		["@keyword.return"] = { fg = c.red },
-		["@operator"] = { fg = c.fg },
-		["@type"] = { fg = c.yellow },
-		["@type.builtin"] = { fg = c.yellow },
-		["@property"] = { fg = c.blue },
-		["@comment"] = { fg = c.gray, italic = true },
-		["@tag"] = { fg = c.blue },
-		["@tag.attribute"] = { fg = c.yellow },
-		["@tag.delimiter"] = { fg = c.fg },
-	}
-
-	for group, settings in pairs(highlights) do
-		vim.api.nvim_set_hl(0, group, settings)
-	end
-end
-
-M.setup()
-
-return M
+return {
+	bg = "none",
+	fg = "#f2f2f2",
+	red = "#ff0080",
+	green = "#79ff0f",
+	blue = "#2d70ff",
+	yellow = "#ffee00",
+	magenta = "#b449be",
+	cyan = "#66ccff",
+	Lgray = "#bbbbbb",
+	Dgray = "#414141",
+	orange = "#ffa600",
+}
---
+++ lua/marios/init.lua
@@ -0,0 +1,4 @@
+function M.setup()
+    require("marios.highlights").setup(
+        require("marios.colors")
+    )
